8e127b8cbb7052d648895acea4c51d661c456cfb
Will <duh102@gmail.com>
2014-04-17 17:16:34 -0400

    Some early db work

--- app.js
+++ app.js
@@ -11,10 +11,20 @@ var request = request.defaults({jar: true});
 
 var mySaltyBucks = null;
 var baseLine = null;
+var db = new sqlite3.Database('./salty.db');
+var players = {};
 
 request.post('http://www.saltybet.com/authenticate?signin=1')
   .form({email: config.email, pword: config.password, authenticate: "signin"});
 
+db.exec('CREATE TABLE IF NOT EXISTS fight(p1 INTEGER, p2 INTEGER, winner INTEGER, p1amount INTEGER, p2amount INTEGER, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(p1) REFERENCES player(id), FOREIGN KEY(p2) REFERENCES player(id));'
+       +'CREATE TABLE IF NOT EXISTS player(id INTEGER primary key, name TEXT)', function(err) {
+  if(err) return console.log(err);
+});
+
+db.each('SELECT id, name FROM player ORDER BY id', function(err, row) {
+  players[row.name] = row.id;
+});
 
 sock.on("message", function(data) {
   updateState();
@@ -52,8 +62,45 @@ function updateState() {
   request("http://www.saltybet.com/state.json", function(e,r,body) {
     console.log(body);
     var s = JSON.parse(body);
+    //betting things
     if(false && s.status == "open") {
       placeBet();
+    }
+    //logging outcomes
+    else if(s.status === "1" || s.status === "2") {
+      //keeping the in-memory hash up to date
+      if(typeof players[s.p1name] === 'undefined') {
+        db.prepare('INSERT INTO player (name) VALUES ($name)', {$name: s.p1name}, function(err) {
+          if(err) console.log(err);
+        }).get(function(err, row) {
+          if(err) console.log(err);
+        })
+        db.prepare('SELECT id FROM player WHERE name = $name', {$name: s.p1name}, function(err) {
+          if(err) console.log(err);
+        }).get(function(err, row) {
+          if(err) console.log(err);
+          players[s.p1name] = row.id;
+        });
+        console.log(players)
+      }
+      if(typeof players[s.p2name] === 'undefined') {
+        db.prepare('INSERT INTO player (name) VALUES ($name)', {$name: s.p2name}, function(err) {
+          if(err) console.log(err);
+        }).exec(function(err) {
+          if(err) console.log(err);
+        })
+        db.prepare('SELECT id FROM player WHERE name = $name', {$name: s.p2name}, function(err) {
+          if(err) console.log(err);
+        }).get(function(err, row) {
+          if(err) console.log(err);
+          players[s.p2name] = row.id;
+        });
+      }
+      var stmnt = db.prepare('INSERT INTO fight (p1, p2, winner, p1amount, p2amount) VALUES ($player1, $player2, $winner, $p1amount, $p2amount)',
+        {$player1: players[s.p1name], $player2: players[s.p2name], $winner: s.status==="1"? 1:2, $p1amount: s.p1total, $p2amount: s.p2total},
+        function(err) {
+          if(err) console.log(err);
+        }).get();
     }
   });
 
@@ -74,7 +121,21 @@ function updateState() {
 
 if(config.serveApi) {
   http.createServer(function(req,res) {
-    res.write(JSON.stringify({salt: mySaltyBucks}));
+    var ranking ={};
+    var matchrecord = {};
+    var i =0;
+    db.each('SELECT name, COUNT(*) as wins FROM player, fight WHERE (p1 = player.id AND winner = 1) OR (p2 = player.id AND winner = 2) GROUP BY player.id ORDER BY wins DESC',
+      function(err, row) {
+        if(err) console.log(err);
+        ranking[i++] ={name: row.name, wins: row.id};
+    });
+    i=0;
+    db.each('SELECT COUNT(*) as matches, p1.name as p1name, p2.name as p2name, winner FROM fight, player AS p1, player AS p2 WHERE p1 = p1.id AND p2 = p2.id GROUP BY p1, p2, winner',
+      function(err, row) {
+        if(err) console.log(err);
+        matchrecord[i++] ={wins: row.matches,p1: row.p1name, p2: row.p2name, winner: row.winner};
+    });
+    res.write(JSON.stringify({salt: mySaltyBucks, players: players, ranking: ranking, matchrecord: matchrecord}));
     res.end();
   }).listen(config.apiPort);;
 }
